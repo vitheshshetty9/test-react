03d13a6f92e681d0affb802cdf692b50d21baa69
vitheshshetty9 <114874729+vitheshshetty9@users.noreply.github.com>
2022-10-04 21:05:16 +0530

    Update pop.jsx

--- src/pop.jsx
+++ src/pop.jsx
@@ -107,7 +107,7 @@ function Pop() {
               </h2>
               <h1 className="h2">Register here</h1>
               <div className="box">
-                <span className="enter">Enter Email: </span>
+                <span className="enter">Enter your Email: </span>
                 <div>
                   <input
                     className="inp"
