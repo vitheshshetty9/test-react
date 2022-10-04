53b41546ad5ce409b68f6c29099034750c0f8ac4
mashshetty <vitheshshetty555@gmail.com>
2022-10-04 20:20:28 +0530

    new change

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ const App = () => {
 
   return (
     <div className="bd">
-      <nav className="nav"><p className="p">Popup</p>
+      <nav className="nav"><p className="p">Popups</p>
       <span className="log">Logout  <span className="oo"> <AiOutlineLogout/></span></span>
       </nav>
       <h1 className="h1">Welcome User <AiOutlineUserAdd />  !!</h1>
